83729b6c9442379b57dc01a581064575a593be89
kadenyi <treykadenyi@gmail.com>
2023-02-09 22:16:10 +0300

    Create new component to implement another method of text transformation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TransformText from './Components/TransformText/TransformText';
+// import TransformText from './Components/TransformText/TransformText';
+import Transform2 from './Components/TransformText/Transform2';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TransformText />} />
+        {/* <Route path="/" element={<TransformText />} /> */}
+        <Route path="/" element={<Transform2 />} />
       </Routes>
     </Router>
   );
